3c94af645a7f8565f3a8ab28b5cbf5e5b2b0896a
Luisa <luisatrece@gmail.com>
2021-03-07 20:08:24 +0100

    Structure for Edit

--- bookstore-client/src/component/book/EdditBookComponent.tsx
+++ bookstore-client/src/component/book/EdditBookComponent.tsx
@@ -1,1 +1,36 @@
-export {};
+import React, { FormEvent, useRef} from 'react'
+const BOOK_API_BASE_URL = "http://localhost:8080/books";
+import axios from 'axios';
+
+const EdditBookComponent: React.FC = () =>{
+
+    const saveBook = () =>{
+        
+    }
+    const txtTitle = "value";
+    const txtAuthor = "value";
+
+    return(
+        <nav className="panel">
+                    <p className="panel-heading">
+                        Add new Book
+                    </p>
+                    <div className = "field">
+                        <label className="label">Title</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Text input" ref={txtTitle} />
+                            </div>
+
+                        <label className="label">Author</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Text input" ref={txtAuthor} />
+                            </div>
+                        <br></br>
+
+                        <button className="button is-primary" onClick={saveBook }> Accept</button>
+                    </div>
+                </nav>
+    );
+}
+
+export default EdditBookComponent
